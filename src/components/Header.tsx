7a1bea344fb2071b7e4cc66b4ca80122b49ab793
Maria Eduarda P. de Almeida <eduardapalm27@gmail.com>
2024-05-20 22:42:44 -0300

    chore: Update Header component styles for responsive navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,10 @@ import { disablePageScroll, enablePageScroll } from "scroll-lock";
 import { useLocation } from "react-router-dom";
 import { navigation } from "../constants";
 import { useState } from "react";
-import edunelLogo from "../assets/edunelLogo.png";
 import { Button } from "./ui/button";
 import { TextAlignJustifyIcon, Cross1Icon } from "@radix-ui/react-icons";
+
+import { IoFlashSharp } from "react-icons/io5";
 import mobileMenuBg from "../assets/svg/mobileMenuBg.svg";
 
 const Header = () => {
@@ -30,13 +31,14 @@ const Header = () => {
 
   return (
     <div
-      className={`fixed top-0 left-0 w-full z-50 border-b lg:backdrop-blur-sm ${
+      className={`fixed top-0 left-0 w-full z-50 border-b lg:backdrop-blur-lg font-header text-color-2 ${
         openNavigation ? "" : "backdrop-blur-sm"
       }`}
     >
       <div className="flex items-center justify-between px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-        <a href="#banner" className="block w-[4rem] xl:mr-8">
-          <img src={edunelLogo} alt="EduNel Logo" width={60} height={60} />
+        <a href="#banner" className="xl:mr-8 flex items-center justify-center">
+          <IoFlashSharp className="text-2xl pt-1" color="#EC651D" />
+          <h1 className="text-color-1 text-2xl font-extrabold">Edunel</h1>
         </a>
 
         <nav
@@ -44,11 +46,11 @@ const Header = () => {
             openNavigation
               ? "flex flex-col text-center h-full pt-[12rem] lg:flex-row lg:pt-0"
               : "hidden"
-          } fixed top-[5rem] left-0 right-0 bottom-0  lg:static lg:flex lg:mx-auto lg:bg-transparent text-neutral-700 overflow-hidden`}
+          } fixed top-[5rem] left-0 right-0 bottom-0  lg:static lg:flex lg:ml-auto lg:mr-4 lg:bg-transparent text-neutral-700 overflow-hidden bg-white`}
         >
           {openNavigation && (
             <img
-              className="absolute top-0 opacity-50 lg:hidden"
+              className="absolute top-0 opacity-20 lg:hidden"
               src={mobileMenuBg}
               alt=""
             />
@@ -58,9 +60,9 @@ const Header = () => {
             <a
               key={item.id}
               href={item.url}
-              className={`block relative text-2xl uppercase transition-colors ${
+              className={`relative transition-colors ${
                 item.onlyMobile ? "lg:hidden" : ""
-              } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs font-semibold ${
+              } px-6 py-6 md:py-8 lg:-mr-0.25 text-md font-semibold ${
                 item.url === pathName.hash ? "z-2" : ""
               } lg:leading-5 xl:px-12`}
               onClick={handleClick}
