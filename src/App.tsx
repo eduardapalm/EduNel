88f6ea614754930b886387c874b0fe9c25418eec
Maria Eduarda P. de Almeida <eduardapalm27@gmail.com>
2024-05-20 22:44:11 -0300

    feat: Add Banner component and update Header styles
    The code changes include adding a new `Banner` component in the `src/components/Banner.tsx` file and updating the styles of the `Header` component. The `Banner` component renders a banner section with a title, description, and image, enhancing the visual appeal and messaging of the website's landing page. The `Header` component styles are updated to improve the responsiveness of the navigation menu. These changes aim to enhance the user experience and improve the overall design of the website.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import Banner from "./components/Banner";
 import Header from "./components/Header";
 
 export default function App() {
   return (
     <>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden w-full h-full">
+      <div className="bg-gradient-to-b from-slate-50 via-color-3 via-85% to-slate-100 pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden w-full h-full">
         <Header />
+        <Banner />
       </div>
     </>
   );
