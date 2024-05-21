27fda3a4b314342bb07e9cce54f848a29ae3d3c1
Maria Eduarda P. de Almeida <eduardapalm27@gmail.com>
2024-05-20 22:43:20 -0300

    chore: Update navigation in constants/index.ts
    This commit updates the navigation in the constants/index.ts file. It adds a new navigation item for "Início" with a corresponding URL. It also updates the IDs and titles of existing navigation items. These changes are made to improve the navigation menu on the website.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,16 +3,21 @@ import INavigationItem from "../interfaces/INavigationItem";
 export const navigation: INavigationItem[] = [
   {
     id: "0",
+    title: "Início",
+    url: "#banner",
+  },
+  {
+    id: "1",
     title: "Nossos Serviços",
     url: "#services",
   },
   {
-    id: "1",
+    id: "2",
     title: "Sobre Nós",
     url: "#about",
   },
   {
-    id: "2",
+    id: "3",
     title: "Entre em Contato",
     url: "#contact",
     onlyMobile: true,
