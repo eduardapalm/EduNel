1a98ec182876080112bf6823bc34e6a8a2daec35
Maria Eduarda P. de Almeida <eduardapalm27@gmail.com>
2024-05-26 00:17:53 -0300

    feat: Comment out AboutUs component in App.tsx
    The AboutUs component in the App.tsx file is commented out to temporarily remove it from the rendered page. This change is made to address a bug or issue related to the AboutUs component.

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ export default function App() {
       <div className="bg-gradient-to-b from-slate-50 via-color-3 via-85% to-slate-100 pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden w-full h-full">
         <Header />
         <Banner />
-        <AboutUs />
+        {/* <AboutUs /> */}
       </div>
     </>
   );
